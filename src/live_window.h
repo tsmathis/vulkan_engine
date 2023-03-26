8d0bb799d6d84d287f1656b0555fb390ff4dff85
Tyler Mathis <35553152+tsmathis@users.noreply.github.com>
2023-03-25 20:16:21 -0700

    Feat: add swapchain creation implementation

--- src/live_window.h
+++ src/live_window.h
@@ -16,6 +16,8 @@ namespace live {
 
 		bool shouldClose() { return glfwWindowShouldClose(window); }
 
+		VkExtent2D getExtent() { return { static_cast<uint32_t>(width), static_cast<uint32_t>(height) }; }
+
 		void createWindowSurface(VkInstance instance, VkSurfaceKHR* surface);
 
 	private:
