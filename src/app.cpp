e5f42ff06d0e77df6377c8f260cbf515f302d74e
Tyler Mathis <35553152+tsmathis@users.noreply.github.com>
2023-04-15 19:07:51 -0700

    Feat: Add support for uniform buffer creation

--- src/app.cpp
+++ src/app.cpp
@@ -15,11 +15,27 @@
 
 
 namespace live {
+	struct GlobalUniformBufferObj {
+		glm::mat4 projectionView{ 1.0f };
+		glm::vec3 lightDirection = glm::normalize(glm::vec3{ 1.0f, -3.0f, -1.0f });
+	};
+
 	Application::Application() { loadObjects();	}
 
 	Application::~Application() {}
 
 	void Application::run() {
+		Buffer globalUniformBuffer{
+			liveDevice,
+			sizeof(GlobalUniformBufferObj),
+			LiveSwapChain::MAX_FRAMES_IN_FLIGHT,
+			VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
+			VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT,
+			liveDevice.properties.limits.minUniformBufferOffsetAlignment
+		};
+
+		globalUniformBuffer.map();
+
 		RenderSystem renderSystem{liveDevice, renderer.getSwapChainRenderPass()};
 		Camera camera{};
 		camera.setViewTarget(glm::vec3(-1.0f, -2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 2.5f));
@@ -43,6 +59,15 @@ namespace live {
 			camera.setPerspectiveProjection(glm::radians(50.0f), aspect, 0.1f, 10.0f);
 			
 			if (auto commandBuffer = renderer.beginFrame()) {
+				int frameIndex = renderer.getFrameINdex();
+
+				//Update
+				GlobalUniformBufferObj uniformBufferObj{};
+				uniformBufferObj.projectionView = camera.getProjectionMatrix() * camera.getViewMatrix();
+				globalUniformBuffer.writeToIndex(&uniformBufferObj, frameIndex);
+				globalUniformBuffer.flushIndex(frameIndex);
+
+				// Render
 				renderer.beginSwapChainRenderPass(commandBuffer);
 				renderSystem.renderObjects(commandBuffer, objects, camera);
 				renderer.endSwapChainRenderPass(commandBuffer);
@@ -56,11 +81,20 @@ namespace live {
 	void Application::loadObjects() {
 		std::shared_ptr<Model> model = Model::createModelFromFile(liveDevice, "models/flat_vase.obj");
 
-		auto loadedModel = Object::createObject();
-		loadedModel.model = model;
-		loadedModel.transform.translation = { 0.0f, 0.5f, 2.5f };
-		loadedModel.transform.scale = { 1.5f, 1.5f, 1.5f };
+		auto flatVase = Object::createObject();
+		flatVase.model = model;
+		flatVase.transform.translation = { -0.5f, 0.5f, 2.5f };
+		flatVase.transform.scale = { 1.5f, 1.5f, 1.5f };
 
-		objects.push_back(std::move(loadedModel));
+		objects.push_back(std::move(flatVase));
+
+		model = Model::createModelFromFile(liveDevice, "models/smooth_vase.obj");
+
+		auto smoothVase = Object::createObject();
+		smoothVase.model = model;
+		smoothVase.transform.translation = { 0.5f, 0.5f, 2.5f };
+		smoothVase.transform.scale = { 3.0f, 1.5f, 3.0f };
+
+		objects.push_back(std::move(smoothVase));
 	}
 }
